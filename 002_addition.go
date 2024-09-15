d122409c2e38cde3e54fbe5cdd711cb753df4d3e
Adipta Martulandi <adipta.martulandi@ruangguru.com>
2024-09-15 16:38:43 +0700

    Add program to sum two numbers

---
+++ 002_addition.go
@@ -0,0 +1,18 @@
+/*
+Membuat sebuah program untuk menjumlahkan 2 bilangan
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	// deklarasi variabel
+	var number1 int = 10
+	var number2 int = 20
+	var result int
+
+	// operasi penjumlahan
+	result = number1 + number2
+	fmt.Println("Hasil penjumlahan", number1, "dan", number2, "adalah", result)
+}
